d048000f4460a5ceb7a7b4da0bf4b4ca43d01655
Prabhnoor Bal <BalPrabhnoor02@gmail.com>
2026-04-23 16:33:26 -0500

    Add Auth flow and backup URL setup

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <p>
+      this is an authenticated route, you should be able to see this only if you are signed in.
+    </p>
+  )
+}
